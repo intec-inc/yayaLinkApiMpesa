756ec0d7a42fa4626c9e97bb2e89ee07d0f574f5
intec-inc <connect.inkluzon@gmail.com>
2025-09-02 20:47:32 +0300

    SD

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const db = fs.firestore();
 
 //----AllOW ACCESS -----//
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
